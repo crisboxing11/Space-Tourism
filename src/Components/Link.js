407fc3c5164b2877cd08106412a8f523323ae9fe
crisboxing11 <crisboxing11@gmail.com>
2022-02-09 21:22:06 -0800

    Added Meta Keys

--- src/Components/Link.js
+++ src/Components/Link.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Link = ({className, href, children}) => {
-  
+  const onClick = (e) => {
+    if (e.metaKey || e.metaKey) {
+      return;
+    }
+  }
 }
 
 export default Link;
