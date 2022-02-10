f1bf481a5d27aac2d5ca8fc062abd9710bef3865
crisboxing11 <crisboxing11@gmail.com>
2022-02-09 21:24:14 -0800

    Added Meta Keys

--- src/Components/Link.js
+++ src/Components/Link.js
@@ -4,7 +4,13 @@ const Link = ({className, href, children}) => {
   const onClick = (e) => {
     if (e.metaKey || e.metaKey) {
       return;
-    }
+    };
+
+    e.preventDefault();
+    window.history.pushState({},"", href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   }
 }
 
