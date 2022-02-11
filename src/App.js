26c27b460cd85af1fc94e8c46237341fbe3a0cb7
crisboxing11 <crisboxing11@gmail.com>
2022-02-10 19:56:12 -0800

    Added Logos

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ const App = () =>  {
   return (
     <Router>
       < Nav />
-      <Routes>
+      {/* <Routes>
         <Route exact path="/" component={Home} />
         <Route exact path="/destination" component={Destination} />
         <Route exact path="/crew" component={Crew} />
         <Route exact path="/technology" component={Technology} />
-      </Routes>
+      </Routes> */}
     </Router>
   );
 }
