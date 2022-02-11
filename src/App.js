934b0cc0bb7bfbea5c86550bca8c48946532148c
crisboxing11 <crisboxing11@gmail.com>
2022-02-10 20:29:36 -0800

    Added Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Nav from './Components/Nav';
+import Home from './Components/Home'
 
 
 const App = () =>  {
   return (
     <Router>
       < Nav />
+      <Home/>
       {/* <Routes>
         <Route exact path="/" component={Home} />
         <Route exact path="/destination" component={Destination} />
