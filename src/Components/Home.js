224bce34c52e602a836c2857f064e023d0a9e067
crisboxing11 <crisboxing11@gmail.com>
2022-02-10 20:26:27 -0800

    Added Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,7 +14,12 @@ const Home = () => {
       <section className="home">
           <div className="overlay">
               <h1>SO YOU WANT TO TRAVEL TO <span>SPACE</span></h1>
-              <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                 <p>
+                  Let’s face it; 
+                  if you want to go to space, 
+                  you might as well genuinely go to outer space and not hover kind of on the edge of it. 
+                  Well sit back, and relax because we’ll give you a truly out of this world experience!
+                  </p>
           </div>
       </section>
   )
