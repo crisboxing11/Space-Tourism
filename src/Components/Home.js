5502f64865409496d89f47d8ce032700fabafc26
crisboxing11 <crisboxing11@gmail.com>
2022-02-10 20:24:10 -0800

    Added Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,3 +7,17 @@ const Home = () => {
 }
 
 export default Home;
+import React from 'react';
+
+const Home = () => {
+  return (
+      <section className="home">
+          <div className="overlay">
+              <h1>SO YOU WANT TO TRAVEL TO <span>SPACE</span></h1>
+              <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+          </div>
+      </section>
+  )
+}
+
+export default Home;
