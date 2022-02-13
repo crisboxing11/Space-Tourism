97e08754e8aa732143113b44213b8ff59af259cc
crisboxing11 <crisboxing11@gmail.com>
2022-02-12 21:04:34 -0800

    cleaning out structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Nav from './Components/Nav';
+import {Crew,Destination,Technology} from './pages';
 import Home from './Components/Home'
 
 
@@ -13,10 +14,13 @@ const App = () =>  {
           <Home />
         </Route>
         <Route path="/destination">
+          <Destination />
           </Route>
         <Route path="/crew">
+          <Crew />
           </Route>
-        <Route path="/tech">  
+        <Route path="/technology">
+          <Technology />
           </Route>
       </Routes>
     </Router>
