40183c46b96639a6c9bb1a4f819e8616eb30e96f
crisboxing11 <crisboxing11@gmail.com>
2022-02-12 21:14:46 -0800

    cleaning out structure

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Destination = () =>{
-  return <div></div>;
+  return <div>Destination Page</div>;
 }
 
 export default Destination;
--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Technology = () => {
-  return <div></div>;
+  return <div>Technology Page</div>;
 }
 
 export default Technology;
