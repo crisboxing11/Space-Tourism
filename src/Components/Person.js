38e19c4bfca81cc609fdec6f4d3ce4182ee635a8
crisboxing11 <crisboxing11@gmail.com>
2022-02-12 20:56:07 -0800

    cleaning out structure

--- src/Components/Person.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Person = () => {
-  return <div>Hello Person</div>;
-}
-
-export default Person;
--- src/Components/Route.js
+++
@@ -1,9 +0,0 @@
-import {useState,useEffect} from 'react';
-
-const Route = () =>  {
-  return <div></div>;
-}
-
-export default Route;
-
-
