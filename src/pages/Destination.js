26dd15edbe6f545715e44f2a1dd631eb99cceb76
crisboxing11 <crisboxing11@gmail.com>
2022-02-14 17:49:35 -0800

    configuring destination logic

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -36,6 +36,22 @@ return (
               </li>
             ))}
           </ul>
+          <h2 className="text-white font-bold text-4xl lg:text-6xl uppercase tracking-widest mb-5">
+            {name}
+          </h2>
+
+          <p className="text-white border-b border-gray-400 pb-5">
+            {description}
+          </p>
+
+          <ul className="flex flex-wrap items-center justify-between pt-5">
+            <li className="uppercase text-gray-200 text-xl">
+              Avg. Distance<span className="text-4xl block">{distance}</span>
+            </li>
+            <li className="uppercase text-gray-200 text-xl">
+              Est. travel time<span className="text-4xl block">{travel}</span>
+            </li>
+          </ul>
         </article>
       </div>
     </section>
