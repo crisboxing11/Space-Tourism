d54f51c4990ab13431ac52524b92a18df4c683f2
crisboxing11 <crisboxing11@gmail.com>
2022-02-14 17:37:03 -0800

    configuring destination logic

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -5,6 +5,26 @@ const Destination = () => {
 const [planets] = useState(data.destinations);
 const [value, setValue] = useState(0);
 const { name, images, description, distance, travel } = planets [value];
+return (
+  <>
+    <section className="home destination">
+      <div className="px-5 pt-32 grid grid-cols-1 gap-5 md:grid-cols-2 gap-10 xl:max-w-7xl xl:mx-auto">
+        <article className="pt-10 xl:pt-32 text-center md:text-left">
+          <h2 className="text-white text-3xl font-extralight text-gray-300 uppercase tracking-wider pb-10">
+            01
+            <span className="ml-5 font-bold">Destination</span>
+          </h2>
+          <img
+            src={images.png}
+            alt={name}
+            title={name}
+            className="block mx-auto"
+          />
+        </article>
+      </div>
+    </section>
+  </>
+);
 };
 
 export default Destination;
